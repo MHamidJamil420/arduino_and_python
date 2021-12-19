d8d86e40fef4f6d5b0696b014d2f7a9448d55fe1
M Hamid Jamil <55445517+MHamidJamil420@users.noreply.github.com>
2021-12-19 20:01:45 +0500

    mainfile

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+import cv2
+from cvzone.FaceDetectionModule import FaceDetector
+cap = cv2.VideoCapture(1)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+detector = FaceDetector()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while True:
+    success, img = cap.read()
+    img, bboxs = detector.findFaces(img)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
